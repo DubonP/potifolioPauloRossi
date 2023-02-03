87ac687ab5b695993cdc8cc8d3e07b221a28810b
PauloRossi <paulogustavorossi@gmail.com>
2023-02-03 03:45:19 -0300

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,3 +21,30 @@ function App() {
 }
 
 export default App;
+
+/* import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+import PaginaInicio from "./paginas/Inicio.pagina";
+import PaginaFavoritos from "./paginas/Favoritos.pagina";
+import PaginaDetalhe from "./paginas/Detalhe.pagina";
+import Cabecalho from "./componentes/layout/cabecalho.componente";
+import store from './state/store';
+import { Provider } from 'react-redux';
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+      <Cabecalho />
+      <Routes>
+        <Route path="/" element={<PaginaInicio />} />
+        <Route path="favoritos" element={<PaginaFavoritos />} />
+        <Route path="detalhe" element={<PaginaDetalhe />} />
+      </Routes>
+      </Provider>
+    </div>
+  );
+}
+
+export default App; */
