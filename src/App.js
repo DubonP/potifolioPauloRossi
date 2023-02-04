3876d25fb5c840c6a87eec02a3d6c6c25c331024
PauloRossi <paulogustavorossi@gmail.com>
2023-02-04 17:29:01 -0300

    creating data and bootstrap header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/home";
+import Header from "./components/header/header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>teste</h1>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
---
+++ src/data/projetos.jsx
@@ -0,0 +1,20 @@
+export const projetos = [
+  {
+    id: "1",
+    titulo: "Site de aluguel de carros",
+    link: " www.google.com.br",
+    data: "27/10/2021",
+    tecnologias: "Javascript, React, Bootstrap, CSS, HTML",
+    image:
+      "https://flyclipart.com/thumb2/computer-icons-logo-brand-javascript-javaserver-pages-free-892749.png",
+  },
+  {
+    id: "2",
+    titulo: "Site para doacao de computadores",
+    link: " www.google.com.br",
+    data: "27/10/2020",
+    tecnologias: "Javascript, React, Bootstrap, CSS, HTML",
+    image:
+      "https://flyclipart.com/thumb2/computer-icons-logo-brand-javascript-javaserver-pages-free-892749.png",
+  },
+];
