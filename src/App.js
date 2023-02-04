4762026169137117a46fb5370c97778715ead907
PauloRossi <paulogustavorossi@gmail.com>
2023-02-03 23:49:10 -0300

    react router dom e algumas pastas

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>teste</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-/* import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import './App.css';
-import PaginaInicio from "./paginas/Inicio.pagina";
-import PaginaFavoritos from "./paginas/Favoritos.pagina";
-import PaginaDetalhe from "./paginas/Detalhe.pagina";
-import Cabecalho from "./componentes/layout/cabecalho.componente";
-import store from './state/store';
-import { Provider } from 'react-redux';
-
-function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-      <Cabecalho />
-      <Routes>
-        <Route path="/" element={<PaginaInicio />} />
-        <Route path="favoritos" element={<PaginaFavoritos />} />
-        <Route path="detalhe" element={<PaginaDetalhe />} />
-      </Routes>
-      </Provider>
-    </div>
-  );
-}
-
-export default App; */
